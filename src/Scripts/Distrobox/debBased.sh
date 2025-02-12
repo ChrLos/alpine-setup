7b621a37a7c634e75c3b3e7a333f686dc376b9ad
ChrLos <lindsey.elizabeta.a4mzm@slmail.me>
2025-02-12 09:20:21 +0800

    fix: New Zen Browser Archived Name

--- src/Scripts/Distrobox/debBased.sh
+++ src/Scripts/Distrobox/debBased.sh
@@ -25,9 +25,9 @@ brow_zen() {
     mkdir -p /home/$user/App/Zen
     chown -R $user:$user /home/$user/App/
 
-    distrobox enter $DISTRO -- bash -c 'sudo curl -s https://api.github.com/repos/zen-browser/desktop/releases/latest | grep "x86_64.*tar.bz2" | cut -d : -f 2,3 | tr -d \" | wget -i - -O /tmp/zenbrowser.tar.bz2'
+    distrobox enter $DISTRO -- bash -c 'sudo curl -s https://api.github.com/repos/zen-browser/desktop/releases/latest | grep "x86_64.*tar" | cut -d : -f 2,3 | tr -d \" | wget -i - -O /tmp/zenbrowser.tar.xz'
 
-    tar -xvf /tmp/zenbrowser.tar.bz2 -C /home/$user/App/Zen --strip-components 1
+    tar -xvf /tmp/zenbrowser.tar.xz -C /home/$user/App/Zen --strip-components 1
 EOF
 
     chmod +x /tmp/zenbrowser.sh
